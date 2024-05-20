7117055ce6a89e99b3d4b1507a589a25d6f9d895
gabimottad <144910507+gabimottad@users.noreply.github.com>
2024-05-19 22:43:00 -0300

    add

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as fs from 'fs';
-import * as express from 'express';
-import * as path from 'path';
 
 async function bootstrap() {
-  const httpsOptions = {
-    key: fs.readFileSync('rootCA.key'),
-    cert: fs.readFileSync('rootCA.pem'),
-    passphrase: 'oseomp23',
-  };
-  const app = await NestFactory.create(AppModule, { httpsOptions });
+  const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
@@ -19,19 +11,13 @@ async function bootstrap() {
     transform: true,
   }));
 
-  const APP_HOSTNAME = 'api-imobiliaria'; 
-
-  app.enableCors({
-    origin: 'http://localhost:8081',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
-
-  app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+  app.enableCors();
 
   const PORT = process.env.PORT || 3005;
   await app.listen(PORT);
-  console.log(`Server running on https://${APP_HOSTNAME}:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 }
 
 bootstrap();
+
+
